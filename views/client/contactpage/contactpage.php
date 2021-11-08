fa7c60e2eb6dfb0dcc621c0e3f568ac5a88ded3f
tan-nguyen1813942 <82354409+tan-nguyen1813942@users.noreply.github.com>
2021-11-08 16:43:31 +0700

    PageContact: /contact

---
+++ views/client/contactpage/contactpage.php
@@ -0,0 +1,94 @@
+<div class="container">
+    <div class="breadcum">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fas fa-home"></i>
+                    Home
+                </a>
+            </li>
+            <li class="active">
+                <a href="#">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="coverContactPage">
+        <div class="contactpage">
+            <div class="col1">
+                <div class="titleContact">
+                    <span>Contact Us</span>
+                </div>
+                <div class="address">
+                    <div class="row1">
+                        <div class="icon">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <span>Address</span>
+                    </div>
+                    <div class="row2">
+                        <p>Bach Khoa Tp.HCM, Dong Hoa, Di An</p>
+                    </div>
+                </div>
+                <div class="phone">
+                    <div class="row1">
+                        <div class="icon">
+                            <i class="fas fa-phone-alt"></i>
+                        </div>         
+                        <span>Phone</span>
+                    </div>
+                    <div class="row2">
+                        <p>Hotline: 0848 806 872</p>
+                    </div>
+                </div>
+                <div class="email">
+                    <div class="row1">
+                        <div class="icon">
+                            <i class="fas fa-envelope-open-text"></i>
+                        </div>
+                        <span>Email</span>
+                    </div>
+                    <div class="row2">
+                        <p>Email: hovaten@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col2">
+                <div class="titleContact">
+                    <span>Tell Us Your Message</span>
+                </div>
+                <form action="post" class="formContact">
+                    <div class="inputform">
+                        <div class="nametext">
+                            <span>Your Name</span> 
+                        </div>
+                        <input type="text">
+                    </div>
+                    <div class="inputform">
+                        <div class="nametext">
+                            <span>Your Email</span>
+                        </div>
+                        <input type="text">
+                    </div>
+                    <div class="inputform">
+                        <div class="nametext">
+                            <span>Title</span>
+                        </div>  
+                        <input type="text">
+                    </div>
+                    <div class="inputMess">
+                        <div class="nametext">
+                            <span>Your Message</span> 
+                        </div>
+                        <textarea name="messContact" id="messContact" cols="30" rows="8"></textarea>
+                    </div>
+                    <button>Send</button>
+                </form>
+            </div>
+            <div class="row3">
+    
+            </div>
+        </div>
+    </div> 
